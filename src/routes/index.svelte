270d10bf9a5d35f8c870a8f36815d9099230f59a
andrewcrobertson <andrewcrobertson@gmail.com>
2020-12-02 21:05:54 +1100

    feat: sapper (#1)

---
+++ src/routes/index.svelte
@@ -0,0 +1,70 @@
+<script type="ts">
+</script>
+
+<style>
+</style>
+
+<svelte:head>
+  <meta name="description" content="Step by Step Music" />
+  <meta
+    name="keywords"
+    content="music, education, guitar, piano, voice, music tuition, violin, drums, guitar teacher, piano teacher, singing, singing teacher, drum teacher" />
+  <meta name="robots" content="index,follow" />
+  <title>Step by Step Music</title>
+</svelte:head>
+
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar" />
+        <span class="icon-bar" />
+        <span class="icon-bar" />
+      </button>
+      <a class="navbar-brand text-hide" href="/">Step by Step Music</a>
+    </div>
+    <div class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li class=""><a href="schools">SCHOOLS</a></li>
+        <li class=""><a href="parents">PARENTS</a></li>
+        <li class=""><a href="contact">CONTACT</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="jumbotron-home" />
+<div class="container-fluid t-content">
+  <div class="row t-page-heading">
+    <div class="col-xs-7 col-sm-4 col-md-4 t-title">
+      <h2>WELCOME</h2>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12">
+        <h3>Welcome!</h3>
+        <p>Step By Step Music offers instrumental music lessons at a number of Victorian primary schools.</p>
+        <p>
+          Our aim is to unlock the musical potential of each and every one of our students, and to prepare them for a lifelong love of music. As countless
+          studies have found, the musical education that an individual receives as a child has a massive impact on their intellectual, academic and social
+          development throughout their life.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+<div id="footer">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xs-12 col-sm-6 col-md-6 t-contact">
+        <p>0404808861</p>
+        <p>info@stepbystepmusic.education</p>
+      </div>
+      <div class="col-xs-12 col-sm-6 col-md-6 t-info">
+        <p class="t-copy">&copy; 2016 Step By Step Music</p>
+        <p><a class="t-credit" href="http://www.tykto.com" target="_blank">Website by Tykto</a></p>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ src/routes/schools.svelte
@@ -0,0 +1,76 @@
+<script type="ts">
+</script>
+
+<style>
+</style>
+
+<svelte:head>
+  <meta name="description" content="Step by Step Music" />
+  <meta
+    name="keywords"
+    content="music, education, guitar, piano, voice, music tuition, violin, drums, guitar teacher, piano teacher, singing, singing teacher, drum teacher" />
+  <meta name="robots" content="index,follow" />
+  <title>Step by Step Music</title>
+</svelte:head>
+
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar" />
+        <span class="icon-bar" />
+        <span class="icon-bar" />
+      </button>
+      <a class="navbar-brand text-hide" href="/">Step by Step Music</a>
+    </div>
+    <div class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="schools">SCHOOLS</a></li>
+        <li class=""><a href="parents">PARENTS</a></li>
+        <li class=""><a href="contact">CONTACT</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="jumbotron-schools" />
+<div class="container-fluid t-content">
+  <div class="row t-page-heading">
+    <div class="col-xs-7 col-sm-4 col-md-4 t-title">
+      <h2>SCHOOLS</h2>
+    </div>
+  </div>
+  <div class="container t-content">
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12">
+        <h3>Step By Step For Your School</h3>
+        <p>
+          Step By Step Music is all about growing together through win-win situations. The right instrumental music program can enhance a schools’ reputation
+          and become a drawcard for prospective parents.
+        </p>
+        <p>
+          We employ dynamic and experienced teachers that are performers in their own right. It is not enough to ‘teach’ children the physicality of playing an
+          instrument, they must also be inspired and challenged by a positive role model that they relate to, and our teachers are selected for this purpose.
+        </p>
+        <p>
+          Step By Step Music integrate and work around the school specialist timetable and events. We encourage students to perform regularly at school
+          assemblies, open nights, Christmas concerts etc, as well as facilitating instrumental performance nights.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+<div id="footer">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xs-12 col-sm-6 col-md-6 t-contact">
+        <p>0404808861</p>
+        <p>info@stepbystepmusic.education</p>
+      </div>
+      <div class="col-xs-12 col-sm-6 col-md-6 t-info">
+        <p class="t-copy">&copy; 2016 Step By Step Music</p>
+        <p><a class="t-credit" href="http://www.tykto.com" target="_blank">Website by Tykto</a></p>
+      </div>
+    </div>
+  </div>
+</div>
