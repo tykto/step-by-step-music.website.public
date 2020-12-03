4754ef7216020b500f151c275b143ac07d47251d
Lara Robertson <laraejrobertson@gmail.com>
2020-12-03 15:02:43 +1100

    refactor: contact components

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,7 @@
   import Jumbotron from './_components/jumbotron.svelte';
   import Heading from './_components/heading.svelte';
   import Content from './_components/content.svelte';
+  import Footer from './_components/footer.svelte';
 </script>
 
 <style>
@@ -23,17 +24,4 @@
   <Heading />
   <Content />
 </div>
-<div id="footer">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-xs-12 col-sm-6 col-md-6 t-contact">
-        <p>0404808861</p>
-        <p>info@stepbystepmusic.education</p>
-      </div>
-      <div class="col-xs-12 col-sm-6 col-md-6 t-info">
-        <p class="t-copy">&copy; 2016 Step By Step Music</p>
-        <p><a class="t-credit" href="http://www.tykto.com" target="_blank">Website by Tykto</a></p>
-      </div>
-    </div>
-  </div>
-</div>
+<Footer />
