6b67d1aa10d79b66c97a91efb8dc7ade985c38a5
Lara Robertson <laraejrobertson@gmail.com>
2020-12-03 14:34:58 +1100

    refactor: home page components

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,8 @@
 <script type="ts">
+  import Nav from './_components/nav.svelte';
+  import Jumbotron from './_components/jumbotron.svelte';
+  import Heading from './_components/heading.svelte';
+  import Content from './_components/content.svelte';
 </script>
 
 <style>
@@ -13,46 +17,11 @@
   <title>Step by Step Music</title>
 </svelte:head>
 
-<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar" />
-        <span class="icon-bar" />
-        <span class="icon-bar" />
-      </button>
-      <a class="navbar-brand text-hide" href="/">Step by Step Music</a>
-    </div>
-    <div class="collapse navbar-collapse">
-      <ul class="nav navbar-nav">
-        <li class=""><a href="schools">SCHOOLS</a></li>
-        <li class=""><a href="parents">PARENTS</a></li>
-        <li class=""><a href="contact">CONTACT</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
-<div class="jumbotron-home" />
+<Nav />
+<Jumbotron />
 <div class="container-fluid t-content">
-  <div class="row t-page-heading">
-    <div class="col-xs-7 col-sm-4 col-md-4 t-title">
-      <h2>WELCOME</h2>
-    </div>
-  </div>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-12 col-md-12">
-        <h3>Welcome!</h3>
-        <p>Step By Step Music offers instrumental music lessons at a number of Victorian primary schools.</p>
-        <p>
-          Our aim is to unlock the musical potential of each and every one of our students, and to prepare them for a lifelong love of music. As countless
-          studies have found, the musical education that an individual receives as a child has a massive impact on their intellectual, academic and social
-          development throughout their life.
-        </p>
-      </div>
-    </div>
-  </div>
+  <Heading />
+  <Content />
 </div>
 <div id="footer">
   <div class="container-fluid">
