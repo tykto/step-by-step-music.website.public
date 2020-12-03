37eb501295eacd0ff17283f5836fea3a6e80349e
Lara Robertson <laraejrobertson@gmail.com>
2020-12-04 10:21:57 +1100

    refactor: footer styles

--- src/components/Footer/index.svelte
+++ src/components/Footer/index.svelte
@@ -1,4 +1,5 @@
 <script type="ts">
+  import drumsticksUrl from './drumsticks.png';
 </script>
 
 <style>
@@ -9,7 +10,7 @@
     background: #333333;
     /* Set the fixed height of the footer here */
     height: 100px;
-    font-size: 14px;
+    font-size: 16px;
     padding-top: 10px;
     color: #ffffff;
     font-family: 'chanticleer_roman_nfregular';
@@ -37,7 +38,7 @@
 
   @media (min-width: 992px) {
     #footer {
-      background: #333333 url(./images/drumsticks.png) no-repeat 40% bottom;
+      background: #333333 var(--drumsticks-url) no-repeat 40% bottom;
     }
 
     .container-fluid {
@@ -46,15 +47,15 @@
   }
 </style>
 
-<div id="footer">
+<div id="footer" style="--drumsticks-url:url({drumsticksUrl})">
   <div class="container-fluid">
     <div class="row">
-      <div class="col-xs-12 col-sm-6 col-md-6 t-contact">
+      <div class="col-xs-12 col-sm-6 col-md-6">
         <p>0404808861</p>
         <p>info@stepbystepmusic.education</p>
       </div>
       <div class="t-info col-xs-12 col-sm-6 col-md-6">
-        <p class="t-copy">&copy; 2016 Step By Step Music</p>
+        <p>&copy; 2016 Step By Step Music</p>
         <p><a href="http://www.tykto.com" target="_blank">Website by Tykto</a></p>
       </div>
     </div>
