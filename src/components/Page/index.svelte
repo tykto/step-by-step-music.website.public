f09ba0e7b0f86b506827603c1553ab13259a051c
laraejrobertson <35183321+laraejrobertson@users.noreply.github.com>
2020-12-04 10:59:15 +1100

    Refactor (#2)
    * refactor: sapper

---
+++ src/components/Page/index.svelte
@@ -0,0 +1,26 @@
+<script type="ts">
+  import Footer from '@app/components/Footer';
+  import Heading from '@app/components/Heading';
+  import Jumbotron from '@app/components/Jumbotron';
+  import Nav from '@app/components/Nav';
+
+  export let active = null;
+  export let jumbotronUrl = null;
+  export let heading = null;
+</script>
+
+<style>
+  .container-fluid {
+    margin-top: -50px;
+  }
+</style>
+
+<Nav {active} />
+<Jumbotron backgroundUrl={jumbotronUrl} />
+<div class="container-fluid">
+  <Heading>{heading}</Heading>
+  <div class="container">
+    <slot />
+  </div>
+</div>
+<Footer />
---
+++ src/pages/Parents/index.svelte
@@ -0,0 +1,24 @@
+<script type="ts">
+  import Page from '@app/components/Page';
+  import jumbotronUrl from './jumbotron.jpg';
+</script>
+
+<style>
+  .row {
+    padding-top: 20px;
+  }
+</style>
+
+<Page heading="PARENTS" active="parents" {jumbotronUrl}>
+  <div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12">
+      <h3>Step By Step For Your Child</h3>
+      <p>
+        Step By Step Music offers Instrumental Music lessons at a number of schools. What is our greatest selling point? Apart from employing highly
+        professional teachers that nurture the musical development of their students in an encouraging and positive environment, the lessons are during school
+        hours. This saves families all that extra running around afterschool taxiing children to lessons. Also because the lessons are at school, it gives the
+        children opportunities to perform at assemblies and other events to show off what all they have learned.
+      </p>
+    </div>
+  </div>
+</Page>
